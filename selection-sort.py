953728fc49ede5d25baefa71bdd92167f7ad62e2
amanuelmandefro2 <128055581+amanuelmandefro2@users.noreply.github.com>
2023-07-03 10:22:26 +0300

    Update selection sort

--- selection-sort.py
+++ selection-sort.py
@@ -1,15 +1,36 @@
+#User function Template for python3
+
 class Solution: 
     def select(self, arr, i):
-        return arr[i]  
+        return arr[i]
     
     def selectionSort(self, arr,n):
+        i = 0;
+
         
-        for i in range(n):
+        while i < n-1:
+            min_elem = self.select(arr, i)
             min_index = i
-            select_elem = self.select(arr, i)
             for j in range(i+1, n):
-                if select_elem > arr[j]:
-                    select_elem = arr[j]
+                if min_elem > arr[j]:
                     min_index = j
-            arr[min_index] = arr[i]
-            arr[i] = select_elem
+                    min_elem = arr[j]
+            
+            (arr[i], arr[min_index]) = (arr[min_index], arr[i])
+            i = i+ 1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        Solution().selectionSort(arr, n)
+        for i in range(n):
+            print(arr[i],end=" ")
+        print()
+# } Driver Code Ends
